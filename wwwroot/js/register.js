02427d68154ee9a85e78a6270bc98582ad71c075
Eldubo <magondubi@gmail.com>
2024-10-28 13:54:01 -0300

    ajustes menores (skibidi sigma cr7 rizz brainrot university), ver commits anteriores

--- wwwroot/js/register.js
+++ wwwroot/js/register.js
@@ -3,17 +3,22 @@ function validarContraseña(){
     let contraseña2 = document.getElementById("contraseña2")
     if(contraseña1 !== contraseña2){
         alert("Las contraseñas no coinciden")
+        return;
     }
     else if(contraseña1.length < 8){
         alert("La contraseña debe tener mas de 8 caracteres")
+        return;
     }
     else if (!/[A-Z]/.test(contraseña1)){
         alert("La contraseña debe tener una letra mayuscula")
+        return;
     }
     else if(!/[!@#$%^&*(),.?":{}|<>]/.test(contraseña1)){
         alert("La contraseña debe tener un caracter especial (!@#$%^&*(),.?:{}|<></>)")
+        return;
     }
     else {
     document.getElementById("registerForm").submit();
+    return;
     }
 }
